8be0a5ff25f3c37e665798a33d1e51d9bf341fd4
Atulin <koumarin@gmail.com>
2022-04-08 12:00:50 +0200

    Added word definitions

--- src/js/info.ts
+++ src/js/info.ts
@@ -1,22 +1,24 @@
-const dialog = document.getElementById('info') as HTMLDialogElement;
+(() => {
+    const dialog = document.getElementById('info') as HTMLDialogElement;
 
-dialog.addEventListener('click', (e) => {
-    const target = e.target as HTMLDialogElement;
-    if (e.offsetX < 0 || e.offsetX > target.offsetWidth ||
-        e.offsetY < 0 && e.offsetY > target.offsetHeight) {
-        dialog.close();
-    }
-});
+    dialog.addEventListener('click', (e) => {
+        const target = e.target as HTMLDialogElement;
+        if (e.offsetX < 0 || e.offsetX > target.offsetWidth ||
+            e.offsetY < 0 && e.offsetY > target.offsetHeight) {
+            dialog.close();
+        }
+    });
 
-document
-    .getElementById('info-button')
-    .addEventListener('click', () => {
-        dialog.showModal();
-    })
+    document
+        .getElementById('info-button')
+        .addEventListener('click', () => {
+            dialog.showModal();
+        })
 
-dialog
-    .querySelector('button#close')
-    .addEventListener('click', () => dialog.close());
+    dialog
+        .querySelector('button#close')
+        .addEventListener('click', () => dialog.close());
+})()
 
 // https://gist.github.com/jbmoelker/226594f195b97bf61436
 interface HTMLDialogElement extends HTMLElement {
