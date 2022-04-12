f2fb4b1c58eac62e603557cd8aef1680211ada9f
Atulin <koumarin@gmail.com>
2022-04-12 10:51:02 +0200

    Added "Liść" and improved the build process with partials. Fixed some CSS stuff too.

--- build.mjs
+++ build.mjs
@@ -5,6 +5,7 @@ import toml from "toml";
 import dayjs from "dayjs";
 import crypto from "crypto";
 import { marked } from 'marked';
+import path from "path";
 
 const ci = console.info;
 
@@ -20,6 +21,12 @@ ci('┃└ Done');
 
 // Create index file
 ci('┠┬ Generating index.html...')
+ci('┃├┬ Registering helpers')
+registerHelpers();
+ci('┃│└ Registered')
+ci('┃├┬ Registering partials')
+await registerPartials();
+ci('┃│└ Registered')
 const index = await render('./src/pages/index.hbs', data);
 ci('┃├ Rendered')
 await fs.writeFile('./dist/index.html', index);
@@ -35,14 +42,24 @@ async function loadData(path) {
     }
 }
 
-async function render(filename, data) {
+function registerHelpers() {
     Handlebars.registerHelper('date', (date, format = null) => dayjs(date.toString().split('(')[0]).format(format));
     Handlebars.registerHelper('dateISO', date => dayjs(date).toISOString());
     Handlebars.registerHelper('toLower', str => str.toLowerCase());
     Handlebars.registerHelper('bust', str => `${str}?=${crypto.randomUUID()}`);
     Handlebars.registerHelper('normalize', str => str.toLowerCase().replace(/\s+/, '_'));
     Handlebars.registerHelper('md', str => marked.parse(str));
+}
 
+async function registerPartials() {
+    const partials = await fs.readdir('./src/partials/');
+    for (const p of partials) {
+        const partial = await fs.readFile(`./src/partials/${p}`);
+        Handlebars.registerPartial(path.parse(p).name, partial.toString());
+    }
+}
+
+async function render(filename, data) {
     const source = await fs.readFile(filename, 'utf8');
     const template = Handlebars.compile(source.toString());
     return template(data);
